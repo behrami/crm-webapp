d4dd0311bf1ee9f67433fc67ce027436324bd7f1
behrami <behram1993@gmail.com>
2018-04-08 19:11:33 -0400

    added contacts

--- crm.rb
+++ crm.rb
@@ -4,3 +4,14 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contactlist = Contact.all
+  erb :contacts
+end
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
